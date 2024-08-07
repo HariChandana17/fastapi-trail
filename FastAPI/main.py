3c497eb7b7b5c406af5cdae94db10e882f525f49
Hari Chandana <chandana1732@gmail.com>
2024-08-07 23:04:00 +0530

    Fastapi trail

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
+# creating an instance of app (u will use this while running (i.e, instance))
 app = FastAPI()
 
 # decorator to -> use the app created
-#"/" stands for base or homepage url
+#"(/)" stands for "Path" (and only slast path is for base or homepage url)
+# "get" refers to the "Operation" on the path
 @app.get('/')
+# decorator is also called 'Path Operation Decorator'
+# below function is named as: 'PATH OPERATION FUNCTION'
 def index():
     return 'heyy'
 
